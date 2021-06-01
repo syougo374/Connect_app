888db188d07b5d8a6a90318eac3e94adbc219c2f
syougo <supernaturalsyougo@gmail.com>
2021-06-01 14:33:32 +0900

    アイコン設定フォーム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:image,:image_cach])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:image,:image_cach])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,

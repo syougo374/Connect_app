9bc880489dddca5bd6116d8d9959aa15953a8ef7
syougo <supernaturalsyougo@gmail.com>
2021-06-01 22:02:51 +0900

    Create:users_controller,Create:tops_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  get 'users/show'
+  get 'tops/index'
+  root "tops#index"
   resources :posts
   devise_for :users
+  resources :users, only: [:show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"

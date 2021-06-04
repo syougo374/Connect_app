6d728ed6149a92532775969a8fd4f9e5e6839589
syougo <supernaturalsyougo@gmail.com>
2021-06-05 00:15:33 +0900

    全体的なcssの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @users = @user.posts.all
   end
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following
+    render 'show_follower'
+  end
 
 end

e5c1c0b54317b96916f6990a1377c643e8519620
syougo <supernaturalsyougo@gmail.com>
2021-06-15 21:24:04 +0900

    #50 Add:post_system_spec実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy ]
   before_action :authenticate_user!, only:[:new, :edit, :update, :destroy]
   before_action :set_q, only: [:index, :search]
 
---
+++ spec/factories/favorite.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :favorite do
+    # association :favorite
+    #  user_id {1}
+    #  post_id {2}
+  end
+end

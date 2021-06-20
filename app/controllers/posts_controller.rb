a465c8f6e92e219167e0eded2648ad55c3880ad2
syougo <supernaturalsyougo@gmail.com>
2021-06-20 20:30:40 +0900

    #60 Fix:ログインしてなくてもpost_showに入れるバグ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy ]
-  before_action :authenticate_user!, only:[:new, :edit, :update, :destroy]
+  before_action :authenticate_user!
   before_action :set_q, only: [:index, :search]
 
   def index
